7acf7f5463bbc3b9540daf34b800bbf443083be0
moliver777 <moliver@i-neda.com>
2013-11-22 18:29:04 +0000

    initialise ball

--- js/ball.js
+++ js/ball.js
@@ -4,4 +4,12 @@ var Ball = function() {
 
 Ball.prototype.init = function() {
 	var ball = this;
+	ball.id = new Date().getTime();
+}
+
+Ball.prototype.teardown = function() {
+	var ball = this;
+	if (ball.interval) clearInterval(ball.interval);
+	ball.interval = null
+	ball = null;
 }
