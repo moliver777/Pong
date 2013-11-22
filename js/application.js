7ee4319b08da1491e48900fdb062280e569a97e8
moliver777 <moliver@i-neda.com>
2013-11-22 17:11:33 +0000

    start and reset buttons

--- js/application.js
+++ js/application.js
@@ -4,4 +4,20 @@ var PongApp = function() {
 
 PongApp.prototype.init = function() {
 	var app = this;
+
+	$("button#start").click(function() {
+		app.start();
+	});
+
+	$("button#reset").click(function() {
+		app.reset();
+	});
+}
+
+PongApp.prototype.start = function() {
+	var app = this;
+}
+
+PongApp.prototype.reset = function () {
+	var app = this;
 }
